e217f6b2c9e44620ac3602338f11fd2e16cdff97
Richard Goulter <richard.goulter@gmail.com>
2014-06-28 10:00:50 +1200

    Added support for tags in posts. Used tutorial from http://javran.github.io/posts/2014-03-01-add-tags-to-your-hakyll-blog.html

--- site.hs
+++ site.hs
@@ -3,6 +3,9 @@
 import           Data.Monoid (mappend)
 import           Hakyll
 
+-- for tags, follow tutorial from:
+-- http://javran.github.io/posts/2014-03-01-add-tags-to-your-hakyll-blog.html
+
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -21,11 +24,28 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
+    -- build up tags
+    tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+
+    tagsRules tags $ \tag pattern -> do
+        let title = "Posts tagged\"" ++ tag ++ "\""
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll pattern
+            let ctx = constField "title" title
+                      `mappend` listField "posts" postCtx (return posts)
+                      `mappend` defaultContext
+            
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/tag.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
+                >>= relativizeUrls
+
     match "posts/*" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
+            >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
             >>= relativizeUrls
 
     create ["archive.html"] $ do
@@ -65,3 +85,7 @@ postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+
+postCtxWithTags :: Tags -> Context String
+postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
