d7dfcf3cc18aff2b8d7e4c836087eceea82ebef3
Richard Goulter <richard.goulter@gmail.com>
2014-06-29 14:20:08 +1200

    Adding a new post script.

---
+++ new_post.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Grabbed from some other Hakyll repo, in particular
+# https://raw.githubusercontent.com/Keruspe/blog/master/new_post.sh
+
+date_pattern=$(date "+%Y-%m-%d")-
+
+read -r -p "Post name > "
+title=${REPLY}
+# to ascii, to lowercase, keep only alphanum and ._- space and turn spaces into dashes
+clean_title=$(echo $title \
+    | iconv -f utf8 -t ascii//translit \
+    | tr '[:upper:]' '[:lower:]' \
+    | tr -dc 'a-z0-9. _-' \
+    | tr ' ' '-')
+
+filename=$date_pattern$clean_title.md
+author=$(git config --get user.name)
+
+cat > "posts/"$filename <<EOF
+---
+title: $title
+author: $author
+tags:
+---
+
+EOF
+
+$EDITOR "posts/"$filename
